121261cf165725c68a14a78f6d19850e9cdeb0d5
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-03-08 13:52:40 -0400

    Implement custom user model and update user references

---
+++ advanced_features_and_security/LibraryProject/relationship_app/signals.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.apps import apps
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import UserProfile
+
+User = apps.get_model(settings.AUTH_USER_MODEL)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
