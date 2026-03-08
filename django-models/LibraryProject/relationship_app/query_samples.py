0d5491e2b5f18fd07273190e2c2197224545b228
AmusaVictorAyo <victoryamusaatlast@gmail.com>
2026-03-08 11:40:05 -0400

    Update sample queries to match checker requirements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,20 +1,25 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="Chinua Achebe")
+author_name = "Chinua Achebe"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
+
 print("Books by specific author:")
 for book in books_by_author:
     print(book.title)
 
 # List all books in a library
-library = Library.objects.get(name="City Library")
+library_name = "City Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
+
 print("\nBooks in library:")
 for book in books_in_library:
     print(book.title)
 
 # Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
+
 print("\nLibrarian for library:")
 print(librarian.name)
